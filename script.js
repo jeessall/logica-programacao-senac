b8b31d7e7fceffe7d01627df27d5fb5427996fce
Jéssica <pedroso.jeh12@gmail.com>
2024-04-06 12:01:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+//var
+//let
+//const
+
+/*let idade = 25
+idade = 15
+
+variavel que pode ser alterada
+
+console.log(idade)*/
+
+/*const idade = 25
+idade = 15
+
+uma variavel que o valor não pode ser alterado
+
+console.log(idade)*/
+
+/*let nome = "Jéssica"
+let idade = 25
+let altura = 1.63
+let aprovado = true
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+console.log(aprovado)*/
+
+/* quando uma variavel declara sem aspas está declarando outra variavel
+let teste = "Jéssica"
+let nome = teste
+let idade = 25
+let altura = 1.63
+let aprovado = true
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+console.log(aprovado)*/
+
+/*let nome = prompt("Qual o seu nome? ")
+let idade = prompt("Qual a sua idade? ")
+let altura = prompt("Qual a sua altura? ")
+let aprovado = prompt("Você foi aprovado? ")
+
+console.log(nome)
+console.log(idade)
+console.log(altura)
+console.log(aprovado)*/
+
+// operadores lógicos
+// == igual valor igual
+// > maior
+// < menor
+// >= maior igual
+// <= menor igual
+// = recebe
+// === identico tipo e valor igual
+
+/*let x = 10
+let y = 20
+
+console.log(x == y)
+console.log(x >= y)
+console.log(x === y)
+console.log(x < y)
+console.log(x <= y)*/
+
+/*operadores matematicos
+soma +
+subtração -
+multiplicação *
+(ao quadrado) exponenciação **
+divisão /
+*/
+
+/*let x = 5
+let y = 5
+
+console.log(x / y)
+console.log(x * y)
+console.log(x - y)
+console.log(x ** y)
+console.log(x + y)*/
+
+//5 valores aleatorios
+//e a media dos valores
+
+/*converter resultado para numero inteiro quando por padrão está recebendo uma string(texto)
+para essa conversão usamos parseInt
+
+let u1 = parseInt(prompt('Valor 1:  '))
+let u2 = parseInt(prompt('Valor 1: '))
+let u3 = parseInt(prompt('Valor 1: '))
+let u4 = parseInt(prompt('Valor 1: '))
+let u5 = parseInt(prompt('Valor 1: '))
+
+let resultado = u1 + u2 + u3 + u4 + u5
+
+console.log((resultado)/5)
+
+//converter resultado para numero decimal parseFloat
+let u1 = parseFloat(prompt('Valor 1:  '))
+let u2 = parseFloat(prompt('Valor 1: '))
+let u3 = parseFloat(prompt('Valor 1: '))
+let u4 = parseFloat(prompt('Valor 1: '))
+let u5 = parseFloat(prompt('Valor 1: '))
+
+let resultado = u1 + u2 + u3 + u4 + u5
+
+console.log((resultado)/5)*/
+
+/*calcular quantas latas de tinta pra pintar um muro
+lata com 3.5l
+cada lata pinta 12m quadrados
+medida usuario que escolhe*/
+
+/*console.log("Cada tinta de 3,5l cobre uma área de 12m2")
+
+let quantidadeDLatas = 3.5
+let medidaMuro = parseInt(prompt('DIgite metros de área a ser pintada? '))
+
+console.log(medidaMuro / 12)*/
+
+//e se quiser colocar a cobertura individual de cada lata?
+
+/*let quantidadeDLatas = 3.5
+let coberturaLata = parseInt(prompt('Quantos metros quadrados a tinta cobre? '))
+let medidaMuro = parseInt(prompt('Digite quantos metros de área a ser pintada? '))
+
+console.log(medidaMuro / coberturaLata)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
