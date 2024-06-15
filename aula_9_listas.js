5ed94920c8650644213753cf5dd457db926073dd
Jéssica <pedroso.jeh12@gmail.com>
2024-06-15 08:41:25 -0300

    Create aula_9_listas.js

---
+++ aula_9_listas.js
@@ -0,0 +1,106 @@
+//lista_compras = [["Carne", ["Promoção", "29,90"]]]
+//console.log(lista_compras [0][1][0]);
+
+//let animal = [{
+   // "nome":"Cavalo",
+  // "peso":"800kg",
+ ///   "altura":"1.45 cm",
+  //  "cor":"Marron",
+ //  "raca": "Crioulo"
+//},
+
+   // console.log(animal['Cavalo'])
+
+
+ //{
+//"nome":"Cachorro",
+//"pesso":"2kg",
+//"altura":"20 cm",
+//"cor":"Marron",
+//"raca":"Caramelo",
+//"filhotes": [
+
+
+    //lista_compras = [["Carne", ["Promoção", "29,90"]]]
+//console.log(lista_compras [0][1][0]);
+
+let animal = [{
+    "nome":"Cavalo",
+   "peso":"800kg",
+    "altura":"1.45 cm",
+    "cor":"Marron",
+   "raca": "Crioulo"
+},
+
+   // console.log(animal['Cavalo'])
+
+
+ {
+"nome":"Cachorro",
+"pesso":"2kg",
+"altura":"20 cm",
+"cor":"Marron",
+"raca":"Caramelo",
+"filhotes": [
+   
+    {
+        "nome":"zezinho 1",
+        "idade":"2",
+    }
+]}]
+
+
+animal[1]['filhotes'].forEach((filho)=> console.log(filho['nome']))
+
+EXERCICIOS
+
+let animal = [ 
+{
+"nome":"cavalo",
+"peso":"800kg",
+"altura":"1.45 cm",
+"cor":"marrom",
+"raça":"crioulo"
+},
+{
+    "nome":"cachorro",
+    "peso":"10kg",
+    "altura": "60 cm",
+    "cor":"branco com preto",
+    "raça":"Border collie",
+    "filhotes": [
+     {
+         "nome":"zezinho 1",
+         "idade": "2",
+     }
+]
+
+console.log(animal[1][1] ['nome'])
+
+
+let animal = [
+    {
+        "nome":"cavalo",
+        "peso":"800kg",
+        "altura":"1.45 cm",
+        "cor":"marrom",
+        "raça":"crioulo"
+    },
+    {
+        "nome":"cachorro",
+        "peso":"10kg",
+        "altura":"60 cm",
+        "cor":"branco com preto",
+        "raça":"Border collie",
+        "filhotes":[
+            {
+                "nome": "zezinho 1",
+                "idade": "2"
+            }
+        ]
+    }
+];
+console.log(animal[1])
+animal[1] ['filhotes'],forEach(filho => {
+    console.log(filho['nome']);  
+});
