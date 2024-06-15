17a14584a7e1dc5a39a89482ccc2bdab9530de23
Jéssica <pedroso.jeh12@gmail.com>
2024-06-15 09:01:48 -0300

    Update aula_9_listas.js

--- aula_9_listas.js
+++ aula_9_listas.js
@@ -104,3 +104,64 @@ console.log(animal[1])
 animal[1] ['filhotes'],forEach(filho => {
     console.log(filho['nome']);  
 });
+
+EXERCICIOS CORRETOS
+
+let animal = [ 
+{
+"nome":"cavalo",
+"peso":"800kg",
+"altura":"1.45 cm",
+"cor":"marrom",
+"raça":"crioulo"
+},
+{
+    "nome":"cachorro",
+    "peso":"10kg",
+    "altura": "60 cm",
+    "cor":"branco com preto",
+    "raça":"Border collie",
+    "filhotes":[ 
+     {
+         "nome":"zezinho 1",
+         "idade": "2",
+     }
+     ]
+ }
+]
+
+console.log(animal[1]) //EXIBIR LISTA POSIÇÃO 1
+animal[1]['filhotes'].forEach(filho => {
+    console.log(filho['nome'])
+})
+
+
+
+// let animal = [
+//     {
+//         "nome":"cavalo",
+//         "peso":"800kg",
+//         "altura":"1.45 cm",
+//         "cor":"marrom",
+//         "raça":"crioulo"
+//     },
+//     {
+//         "nome":"cachorro",
+//         "peso":"10kg",
+//         "altura":"60 cm",
+//         "cor":"branco com preto",
+//         "raça":"Border collie",
+//         "filhotes":[
+//             {
+//                 "nome": "zezinho 1",
+//                 "idade": "2"
+//             }
+//         ]
+//     }
+// ];
+
+// //DENTRO DA LISTA NA POSIÇÃO [1] PEGANDO A LISTA DE FILHOTES DENTRO DA LISTA [1] PROCURANDO PELO NOME DENTRO DA LISTA
+// console.log(animal[1])
+// animal[1] ['filhotes'].forEach(filho => {
+//     console.log(filho['nome']);  
+// });
